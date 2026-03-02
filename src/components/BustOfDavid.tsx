58d81d37928aaef18582b6561b27f068ba8787c1
Henglay <tehenglay098@gmail.com>
2026-03-02 20:45:59 +0700

    Update BustOfDavid.tsx

--- src/components/BustOfDavid.tsx
+++ src/components/BustOfDavid.tsx
@@ -1,5 +1,5 @@
 import { Suspense, useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { useGLTF, OrbitControls, Bounds, Center } from '@react-three/drei';
 import * as THREE from 'three';
 
